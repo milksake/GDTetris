882b1d9add736c2e1c4ee3c71a44efc84919e1c5
joaquin30 <46060104+joaquin30@users.noreply.github.com>
2024-01-23 12:50:21 -0500

    tetris v2

--- tetris/block.gd
+++ tetris/block.gd
@@ -5,6 +5,7 @@ const MAX_VEL_Y := 500.0
 const BLOCK_SIZE := 32
 var active := true
 var screen_size: Vector2
+var remainder: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,15 +17,27 @@ func _ready():
 func _process(dt: float):
 	if active:
 		if Input.is_action_pressed("block_down"):
-			position.y += MAX_VEL_Y * dt
+			remainder.y += MAX_VEL_Y * dt
 		else:
-			position.y += VEL_Y * dt
+			remainder.y += VEL_Y * dt
 		
-		if Input.is_action_just_pressed("block_move_left"):
-			position.x -= BLOCK_SIZE
+		if Input.is_action_pressed("block_move_left"):
+			remainder.x -= MAX_VEL_Y * dt
 		
-		if Input.is_action_just_pressed("block_move_right"):
+		if Input.is_action_pressed("block_move_right"):
+			remainder.x += MAX_VEL_Y * dt
+		
+		while remainder.x > BLOCK_SIZE:
 			position.x += BLOCK_SIZE
+			remainder.x -= BLOCK_SIZE
+		
+		while remainder.x < -BLOCK_SIZE:
+			position.x -= BLOCK_SIZE
+			remainder.x += BLOCK_SIZE
+		
+		while remainder.y > BLOCK_SIZE:
+			position.y += BLOCK_SIZE
+			remainder.y -= BLOCK_SIZE
 		
 		if Input.is_action_just_pressed("block_rotate"):
 			rotation += PI / 2.0
